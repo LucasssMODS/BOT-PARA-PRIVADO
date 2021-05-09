97b0ec00296cf577054b7a9c21e0ed396a631c54
viniciusgdr <79378773+viniciusgdr@users.noreply.github.com>
2021-05-09 20:17:22 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -199,48 +199,45 @@ API: https://api-gdr.herokuapp.com
 ̑ͮͫͭͥͦͣ̔͒͑͑̅͊́͋͊͗͑̓̍̽ͬ̋̎̓ͧ͂̊̉ͣ̉ͩ̀̈̏̍ͭ̒͒͂̍̇̐̿ͨͦ̑͑̃̎͒̋͌̌̈́ͩ̔ͩ̉̿̐ͭ̂͛͊ͩ̔͋ͭͩ͑̆̇͊ͫ̃͋̋ͪͦ͐̈́͗ͭ̒̐̿̃͂́̅̉͊ͧͫ̔̄̿̓́̀̄̾ͨ̍ͨͩͩͮ̑ͭͧͦͣ̽̃͛̔̇ͣ̂͗ͪ͛̈́̆ͮͧ̈̔́͛́͊̇͒ͭ̓ͬ̅̑̊ͩ͆̌ͩ͑̒͊ͭ̀̃̏̅̏ͦͣ̏̿ͬ͂̿ͣ́̓͌ͥ̍ͦ̊̄̆̿̿̈́̅̄̐̎͌͋ͦ͋͂ͨ̂̈͂̉̈́̀̒̃̈́͂̃ͤͪ̎͋̆ͤ̽̇ͩ̑ͨ̽͊ͦͩ̓̎̑̎̆̉ͮ̄͑̆͌ͨ̓̀̋ͤͥͯ͋͑ͧ̆̋ͨͯ̔̑̽͗ͬ͒̀̓͐ͯ̏͆̏ͣ̌͌ͩ̏́̈͂̃͋͛̔ͣ̓̓̇̉ͪͩͯ͛̍͑ͯͨͫͯ̔ͯ̾̓͊͐ͯ̑̚̚̚̚̚�​  
 \n
 *Comandos*
-┇─*${prefix}publico*
-┇─*${prefix}self*
-┇─*${prefix}setprefix*
-┇─*${prefix}bc*
-┇─*${prefix}setthumb*
-┇─*${prefix}fakethumb*
-┇─*${prefix}status*
-┇─*${prefix}bloquear*
-┇─*${prefix}desbloquear*
-┇─*${prefix}entrar*
-┇─*${prefix}hidetag*
-┇─*${prefix}stickertag*
-┇─*${prefix}promover*
-┇─*${prefix}despromover*
-┇─*${prefix}linkgrupo*
-┇─*${prefix}abrirgrp*
-┇─*${prefix}fechargrp*
-┇─*${prefix}mudardesc*
-┇─*${prefix}pegartexto*
-┇─*${prefix}toimage*
-┇─*${prefix}spam*
-┇─*${prefix}adicionar*
-┇─*${prefix}banir*
-┇─*${prefix}chat*
-┇─*${prefix}fakereply*
-┇─*${prefix}lertudo*
-┇─*${prefix}mutar*
-┇─*${prefix}desmutar*
-┇─*${prefix}dellchat*
-┇─*${prefix}arquivar*
-┇─*${prefix}desarquivar*
-┇─*${prefix}term*
-┇─*${prefix}setreply*
-┇─*${prefix}setbio*
-┇─*${prefix}getpic*
-┇─*${prefix}getbio*
-┇─*${prefix}sticker*
-┇─*${prefix}tinyurl*
-┇─*${prefix}semprefixo*
-┇─*${prefix}ytmp3*
-┇─*${prefix}play*
-┇─*${prefix}ytmp4*
+┇─ *${prefix}publico*
+┇─ *${prefix}self*
+┇─ *${prefix}setprefix*
+┇─ *${prefix}bc*
+┇─ *${prefix}setthumb*
+┇─ *${prefix}status*
+┇─ *${prefix}bloquear*
+┇─ *${prefix}desbloquear*
+┇─ *${prefix}entrar*
+┇─ *${prefix}hidetag*
+┇─ *${prefix}stickertag*
+┇─ *${prefix}promover*
+┇─ *${prefix}despromover*
+┇─ *${prefix}linkgrupo*
+┇─ *${prefix}abrirgrp*
+┇─ *${prefix}fechargrp*
+┇─ *${prefix}mudardesc*
+┇─ *${prefix}pegartexto*
+┇─ *${prefix}toimage*
+┇─ *${prefix}adicionar*
+┇─ *${prefix}banir*
+┇─ *${prefix}chat*
+┇─ *${prefix}fakereply*
+┇─ *${prefix}lertudo*
+┇─ *${prefix}mutar*
+┇─ *${prefix}desmutar*
+┇─ *${prefix}arquivar*
+┇─ *${prefix}desarquivar*
+┇─ *${prefix}term*
+┇─ *${prefix}setreply*
+┇─ *${prefix}setbio*
+┇─ *${prefix}getpic*
+┇─ *${prefix}getbio*
+┇─ *${prefix}sticker*
+┇─ *${prefix}tinyurl*
+┇─ *${prefix}semprefixo*
+┇─ *${prefix}ytmp3*
+┇─ *${prefix}play*
+┇─ *${prefix}ytmp4*
 ┇Adicionar Fotos ao Sistema
 ┇
 ┇─*${prefix}addimagem*
@@ -852,7 +849,7 @@ API: https://api-gdr.herokuapp.com
                     wa.sendFakeStatus(from, "*Successo", "GRUPO ABERTO")
                 })
                 break
-            case 'mudarnomegrp':
+            case 'nomegrp':
                 if (!isGroup) return await reply('Só pode ser usado em grps otário!s')
                 if (!isAdmin) return await reply('Só pode ser usado por admin!!!')
                 if (!botAdmin) return await reply('Só Consigo usar com eu sendo ademir')
